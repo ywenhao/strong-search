64e90b1a95282308f0207c9524f5579ba237948a
actions.win <actions.win@foxmail.com>
2023-10-20 11:26:34 +0800

    feat: 添加中文配置

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue'
 import { formatValue, StrongSearch, type SearchValue } from './components/strong-search'
 import { filterList } from './state'
 
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import { ElConfigProvider } from 'element-plus'
+
 const searchValue = ref<SearchValue[]>([])
 
 function handleSearch(item: SearchValue[]) {
@@ -12,10 +16,13 @@ function handleSearch(item: SearchValue[]) {
 </script>
 
 <template>
-  <StrongSearch
-    v-model="searchValue"
-    :filterList="filterList"
-    @search="handleSearch"
-    placeholder="默认按照站点搜索"
-  />
+  <!-- 项目里面配置了这个中文的就不用倒入这个了 -->
+  <ElConfigProvider :locale="zhCn">
+    <StrongSearch
+      v-model="searchValue"
+      :filterList="filterList"
+      @search="handleSearch"
+      placeholder="默认按照站点搜索"
+    />
+  </ElConfigProvider>
 </template>
